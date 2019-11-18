4e23f498db3bc685623a5b744a024b450ffa778d
Dr-Brains <laurens.vercauteren@gmail.com>
2019-11-18 13:50:26 +0100

    created controller integration tests and repository jpa tests

---
+++ domain/src/test/java/be/wailsharks/parkshark/domain/division/TestRunApplication.java
@@ -0,0 +1,16 @@
+package be.wailsharks.parkshark.domain.division;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "be.wailsharks.parkshark")
+//@EnableJpaRepositories(basePackages = "be.wailsharks.parkshark.domain")
+//@EntityScan(basePackages = "be.wailsharks.parkshark.domain")
+public class TestRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestRunApplication.class, args);
+    }
+}
